eb2116fba1d94c452dac16e046c4fd2f9d474525
Christopher Dougherty <christophertdoc@gmail>
2020-04-16 00:07:05 -0400

    response

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 import './App.css'
 
 function App() {
+	const [response, setResponse] = useState([])
+    useEffect(() => {
+		axios.get(`http://taco-randomizer.herokuapp.com/random/`)
+		.then(resp => {
+			if (response.length < 1) {
+				setResponse(response => [resp.data, ...response])
+			}
+			console.log('resp', resp)
+		})
+    }, [response]) 
 	return (
 		<div className="App">
 			<h1>Taco Gen</h1>
